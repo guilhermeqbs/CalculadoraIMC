21b1087e024a671ccdb545aadc82c1ad97ab4103
Guilherme Basílio <guilhermeqbs@gmail.com>
2021-05-17 22:00:24 -0300

    final

---
+++ app/src/main/java/com/example/calculadoraimc/TabelaIMCActivity.java
@@ -0,0 +1,14 @@
+package com.example.calculadoraimc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TabelaIMCActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tabela_i_m_c);
+    }
+}
